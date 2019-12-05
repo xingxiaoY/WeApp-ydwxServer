ac83da54be60076f803624082d7d5fe6545c9b3d
xingxiaoY <894628478@qq.com>
2019-12-05 17:45:54 +0800

    更新

--- src/pages/member/login.js
+++ src/pages/member/login.js
@@ -5,13 +5,13 @@ const App = getApp()
 Page({
 	data: {
 		form: {
-			// userLoginName: '',
+			userLoginName: '',
 			mobile: '',
 		},
 	},
 	onLoad() {
 		this.initValidate()
-		console.log(this.WxValidate)
+		// console.log(this.WxValidate) /*输出用户基本信息*/
 	},
 	showModal(error) {
 		wx.showModal({
@@ -23,63 +23,50 @@ Page({
 		const params = e.detail.value;
 		// console.log(params);
 		const tel = e.detail.value.tel;
-		console.log(tel);
+		// console.log(tel);/*输出用户输入手机号*/
 		// 传入表单数据，调用验证方法
 		if (!this.WxValidate.checkForm(params)) {
 			const error = this.WxValidate.errorList[0]
 			this.showModal(error)
 			return false
 		}else{
-
 			wx.request({
-
 				url: 'https://weixin2.sinoprof.com:8443/api/minipro/loginTest',
-
 				header: {
-
 					"Content-Type": "application/x-www-form-urlencoded"
-
 				},
-
 				method: "post",
-
 				data:{mobile:tel},
 				// data:{mobile:e.detail.value.tel,userLoginName:e.detail.value.userLoginName},
-
 				success: function(res) {
-
-					if(res.data.status == 0){
-
-						wx.showToast({
-
-							title: res.data.code,
-
-							icon: 'loading',
-
-							duration: 1500
-
+					const message = res.data.message; //打印出手机号匹配成功
+					if(message=='请求成功'){
+						console.log(res.data.data.userLoginName);
+						wx.setStorage({
+							key:"userLoginName",
+							data:res.data.data.userLoginName
 						})
-
-
-
+						wx.reLaunch({
+							url:'../index/index',
+							success(res) {
+								// 打开成功
+							}
+						})
 					}else{
-						console.log(res)
 						wx.showToast({
-
-							title: res.data.message,//这里打印出登录成功
-
+							title: res.data.message,//打印出登录成功
 							icon: 'success',
+							duration: 1500,
+							success:function(){
+								const message = res.data.message; //打印出手机号是否匹配
 
-							duration: 1000
 
+							},//接口调用成功
 						})
 
 					}
-
 				}
-
 			})
-
 		}
 	},
 	initValidate() {
@@ -110,6 +97,7 @@ Page({
 		this.WxValidate = new WxValidate(rules, messages)
 	},
 
+	onShow:function(){
 
-
+	}
 })
